5714cd2d96a70e69034ac06193e4fdfd965b48f3
Najim Uddin <najim7304@gmail.com>
2022-12-12 21:51:35 +0600

    function are added two numbers

---
+++ myfunctions/myfunctions.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+function addTwo(num) {
+    return num + 2;
+}
+function getUpper(val) {
+    return val.toLocaleUpperCase();
+}
+console.log(addTwo(3));
+var up = getUpper('Hello');
+console.log(up);
---
+++ myfunctions/myfunctions.ts
@@ -0,0 +1,12 @@
+function addTwo (num:number){
+    return num+2
+}
+function getUpper (val:string){
+    return val.toLocaleLowerCase();
+}
+console.log(addTwo(3));
+const up = getUpper('Hello')
+console.log(up);
+
+
+export{}
