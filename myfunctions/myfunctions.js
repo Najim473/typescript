cd856c781079ba2febd634f060f2fe9d2784b2dc
Najim Uddin <najim7304@gmail.com>
2022-12-12 22:34:17 +0600

    added heros functions in myfunctions

--- myfunctions/myfunctions.js
+++ myfunctions/myfunctions.js
@@ -4,8 +4,23 @@ function addTwo(num) {
     return num + 2;
 }
 function getUpper(val) {
-    return val.toLocaleUpperCase();
+    return val.toLocaleLowerCase();
 }
+function signUpUser(name, email) { }
+// function getValue(myVal: number):boolean {
+//   if (myVal > 5) {
+//     return true;
+//   }
+//   return '200 Ok'
+// }
+var heros = ['Thor', 'Jhon Wick', 'Spiderman'];
+var heross = heros.map(function (hero) {
+    return "hero is ".concat(hero);
+});
 console.log(addTwo(3));
-var up = getUpper('Hello');
+var up = getUpper("Hello");
 console.log(up);
+var signup = signUpUser("Najim", "najim@gmail.com");
+console.log(signup);
+// console.log(getValue(2));
+console.log(heross[1]);
--- myfunctions/myfunctions.ts
+++ myfunctions/myfunctions.ts
@@ -1,12 +1,26 @@
-function addTwo (num:number){
-    return num+2
+function addTwo(num: number) {
+  return num + 2;
 }
-function getUpper (val:string){
-    return val.toLocaleLowerCase();
+function getUpper(val: string) {
+  return val.toLocaleLowerCase();
 }
+function signUpUser(name: string, email: string) {}
+// function getValue(myVal: number):boolean {
+//   if (myVal > 5) {
+//     return true;
+//   }
+//   return '200 Ok'
+// }
+const heros = ['Thor', 'Jhon Wick', 'Spiderman'];
+const heross = heros.map((hero):string=>{
+    return `hero is ${hero}`
+})
 console.log(addTwo(3));
-const up = getUpper('Hello')
+const up = getUpper("Hello");
 console.log(up);
+const signup = signUpUser("Najim", "najim@gmail.com");
+console.log(signup);
+// console.log(getValue(2));
+console.log(heross[1]);
 
-
-export{}
+export {};
